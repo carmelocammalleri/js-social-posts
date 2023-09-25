98b398d3f3b9ed983630b559d631ba69db227654
Melozzo <carmecamma1995@gmail.com>
2023-09-25 15:52:19 +0200

    [edit]: addeventlistner to the btn like

--- js/main.js
+++ js/main.js
@@ -60,38 +60,47 @@ const posts = [
 //2. collegare dall'html i vari punti
 const container = document.getElementById('container');
 
+//3. stampare sull'html gli elementi
 posts.forEach((post)=> {
     container.innerHTML += 
     `
-    <div class="post">
-    <div class="post__header">
-        <div class="post-meta">                    
-            <div class="post-meta__icon">
-                <img class="profile-pic" src="${post.author.image}" alt="Phil Mangione">                    
+        <div class="post">
+            <div class="post__header">
+                <div class="post-meta">                    
+                    <div class="post-meta__icon">
+                        <img class="profile-pic" src="${post.author.image}" alt="Phil Mangione">                    
+                    </div>
+                    <div class="post-meta__data">
+                        <div class="post-meta__author">${post.author.name}</div>
+                        <div class="post-meta__time">${post.created}</div>
+                    </div>                    
+                </div>
             </div>
-            <div class="post-meta__data">
-                <div class="post-meta__author">${post.author.name}</div>
-                <div class="post-meta__time">${post.created}</div>
-            </div>                    
+            <div class="post__text">${post.content}</div>
+            <div class="post__image">
+                <img src="${post.media}" alt="">
+            </div>
+            <div class="post__footer">
+                <div class="likes js-likes">
+                    <div class="likes__cta">
+                        <a class="like-button  js-like-button" href="#" data-postid="${post.id}">
+                            <i class="like-button__icon fas fa-thumbs-up" aria-hidden="true"></i>
+                            <span class="like-button__label">Mi Piace</span>
+                        </a>
+                    </div>
+                    <div class="likes__counter">
+                        Piace a <b id="like-counter-1" class="js-likes-counter">${post.likes}</b> persone
+                    </div>
+                </div> 
+            </div>            
         </div>
-    </div>
-    <div class="post__text">${post.content}</div>
-    <div class="post__image">
-        <img src="${post.media}" alt="">
-    </div>
-    <div class="post__footer">
-        <div class="likes js-likes">
-            <div class="likes__cta">
-                <a class="like-button  js-like-button" href="#" data-postid="${post.id}">
-                    <i class="like-button__icon fas fa-thumbs-up" aria-hidden="true"></i>
-                    <span class="like-button__label">Mi Piace</span>
-                </a>
-            </div>
-            <div class="likes__counter">
-                Piace a <b id="like-counter-1" class="js-likes-counter">${post.likes}</b> persone
-            </div>
-        </div> 
-    </div>            
-</div>
     `
 })
+
+//3. bottone like
+const btnLike = document.querySelector('.like-button')
+btnLike.addEventListener('click', liked)
+
+function liked (){
+    btnLike.classList.toggle('like-button--liked');
+}
